cc9feb90c1d70a1ee8f1b292e274896d7d7bdeaa
hoon2-kim <gooaba1204@gmail.com>
2024-06-24 16:13:25 +0900

    socket.io error-handle

---
+++ socket-error-handle/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('list items', async (callback) => {
+        try {
+            const items = await findItems();
+            callback({
+                status: 'OK',
+                items,
+            });
+        } catch (e) {
+            callback({
+                status: 'NOT OK',
+            });
+        }
+    });
+});
+
+httpServer.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
